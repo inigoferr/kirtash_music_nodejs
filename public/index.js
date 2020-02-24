247133d2b484fb07c0b15dffc3e2268c76332a6e
Inigo Fernandez <fernandez.115986@e.unavarra.es>
2020-02-24 18:26:03 +0100

    go

--- public/index.js
+++ public/index.js
@@ -1809,6 +1809,12 @@ function v_showPlaylist(id_session,req,callback){
                 admin = 0;
             }
             
+            if (admin == 1){
+                template = fs.readFileSync(path.join(__dirname + "/file_html/playlist_admin.html"),'utf-8');
+            } else {
+                template = fs.readFileSync(path.join(__dirname + "/file_html/playlist_noadmin.html"),'utf-8');
+            }
+
             result = "";
 
             num_x_playlist = 0;
@@ -1822,12 +1828,7 @@ function v_showPlaylist(id_session,req,callback){
             console.log("Playlist = " +res[1]);
             i = 1;
             res.forEach(function(datos){
-                if (admin == 1){
-                    template = fs.readFileSync(path.join(__dirname + "/file_html/playlist_admin.html"),'utf-8');
-                } else {
-                    template = fs.readFileSync(path.join(__dirname + "/file_html/playlist_noadmin.html"),'utf-8');
-                }
-
+               
                 content = template;
     
                 content = content.replace(/##number##/g,i);
@@ -1861,11 +1862,6 @@ function v_showPlaylist(id_session,req,callback){
                         }
                     });
                 } else {
-                    if (admin == 1){
-                        template = fs.readFileSync(path.join(__dirname + "/file_html/playlist_admin.html"),'utf-8');
-                    } else {
-                        template = fs.readFileSync(path.join(__dirname + "/file_html/playlist_noadmin.html"),'utf-8');
-                    }
                     content = template;
 
                     content = content.replace(/##disabledadd##/g,"");
