5cd25e22e99934672ceac7ce145fbb46a122a6dd
Inigo Fernandez <fernandez.115986@e.unavarra.es>
2020-02-26 16:08:50 +0100

    Email

--- public/index.js
+++ public/index.js
@@ -2029,13 +2029,13 @@ var mailOptions = {
   text: 'That was easy!'
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+/*transporter.sendMail(mailOptions, function(error, info){
   if (error) {
     console.log(error);
   } else {
     console.log('Email sent: ' + info.response);
   }
-});
+});*/
 
 
 
