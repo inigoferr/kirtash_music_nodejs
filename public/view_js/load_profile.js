2c9f66e1bf836c4e61ec4aa32550f5ed1b9b1d1e
Inigo Fernandez <fernandez.115986@e.unavarra.es>
2020-02-27 14:16:46 +0100

    go

--- public/view_js/load_profile.js
+++ public/view_js/load_profile.js
@@ -1,53 +1,44 @@
-window.onload = function(){
-    
+window.onload = function () {
+
+    //$view->v_showUsername();
     $.ajax({
         type: "post",
-        url: "/checkregistration",
+        url: "/v_showusername",
         success: function (response) {
-            if(response["result"] == 1){
-
-                //$view->v_showUsername();
-                $.ajax({
-                    type: "post",
-                    url: "/v_showusername",
-                    success: function (response) {
-                        $('#box_username').html(response);
-                        $('#username_edit_modal_input').val(response);
-                    }
-                });
-
-                //$view->v_showDescriptionUser();
-                $.ajax({
-                    type: "post",
-                    url: "/v_showDescriptionUser",
-                    success: function (response) {
-                        $('#description-user').html(response);
-                        $('#description_edit_modal').val(response);
-                    }
-                });
-
-                //$view->v_showInfoSessionsUser();
-                $.ajax({
-                    type: "post",
-                    url: "/v_showInfoSessionsUser",
-                    success: function (response) {
-                        console.log("Response = "+response+"/");
-                        $('#info_sessions_user').html(response);
-                    }
-                });
-
-                //$view->v_showActiveSessions();
-                $.ajax({
-                    type: "post",
-                    url: "/v_showActiveSessions",
-                    success: function (response) {
-                        $('#show_active_sessions').html(response);
-                    }
-                });
-
-            } else {
-                window.location = "/index.html";
-            }
+            $('#box_username').html(response);
+            $('#username_edit_modal_input').val(response);
         }
     });
+
+    //$view->v_showDescriptionUser();
+    $.ajax({
+        type: "post",
+        url: "/v_showDescriptionUser",
+        success: function (response) {
+            $('#description-user').html(response);
+            $('#description_edit_modal').val(response);
+        }
+    });
+
+    //$view->v_showInfoSessionsUser();
+    $.ajax({
+        type: "post",
+        url: "/v_showInfoSessionsUser",
+        success: function (response) {
+            console.log("Response = " + response + "/");
+            $('#info_sessions_user').html(response);
+        }
+    });
+
+    //$view->v_showActiveSessions();
+    $.ajax({
+        type: "post",
+        url: "/v_showActiveSessions",
+        success: function (response) {
+            $('#show_active_sessions').html(response);
+        }
+    });
+
+
+}
 }
