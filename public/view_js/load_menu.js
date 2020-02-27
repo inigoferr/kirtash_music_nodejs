3789d567c6583d29b23f6b6952d00d6d2eff3508
Inigo Fernandez <fernandez.115986@e.unavarra.es>
2020-02-27 14:11:39 +0100

    go

--- public/view_js/load_menu.js
+++ public/view_js/load_menu.js
@@ -1,45 +1,34 @@
-window.onload = function(){
+window.onload = function () {
 
+    //$view->v_showUsername();
     $.ajax({
         type: "post",
-        url: "/checkregistration",
+        url: "/v_showusername",
         success: function (response) {
-            if (response["result"] == 1){
-                //$view->v_showUsername();
-                $.ajax({
-                    type: "post",
-                    url: "/v_showusername",
-                    success: function (response) {
-                        icon = "<i class='tim-icons icon-single-02'></i>";
-                        result = icon.concat(response);
+            icon = "<i class='tim-icons icon-single-02'></i>";
+            result = icon.concat(response);
 
-                        $('#button_username').html(result);
-                    }
-                });
-
-                //$view->v_showListSessions(0); 
-                $.ajax({
-                    type: "post",
-                    url: "/v_showlistsessions",
-                    data: {"number":0},
-                    success: function (response) {
-                        $('#session_list').html(response);
-                    }
-                });
-
-                //$view->v_showFollowedSessions(0);
-                $.ajax({
-                    type: "post",
-                    url: "/v_showfollowedsessions",
-                    data: {"number":0},
-                    success: function (response) {
-                        $('#follow_list').html(response);
-                    }
-                });
-            } else {
-                window.location = "/index.html";
-            }
+            $('#button_username').html(result);
         }
     });
 
+    //$view->v_showListSessions(0); 
+    $.ajax({
+        type: "post",
+        url: "/v_showlistsessions",
+        data: { "number": 0 },
+        success: function (response) {
+            $('#session_list').html(response);
+        }
+    });
+
+    //$view->v_showFollowedSessions(0);
+    $.ajax({
+        type: "post",
+        url: "/v_showfollowedsessions",
+        data: { "number": 0 },
+        success: function (response) {
+            $('#follow_list').html(response);
+        }
+    });
 }
